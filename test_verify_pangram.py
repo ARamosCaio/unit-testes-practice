2a1137c02c9405fcf571e2f45f5f0daf3670ec85
Caio Ramos <aramoscaio@gmail.com>
2023-05-26 18:12:48 -0300

    feat: added is not pangram test

--- test_verify_pangram.py
+++ test_verify_pangram.py
@@ -3,9 +3,14 @@ from pangram import verify_pangram
 
 class VerifyPangramTest(unittest.TestCase):
     def test_method(self):
-        #sentence = "The quick brown fox jumps over a lazy dog"
+        
         sentence = "Not a pangram"
         is_pangram = verify_pangram(sentence)
 
         self.assertTrue(is_pangram)
 
+    def test_method(self):
+        sentence = "The quick brown fox jumps over a lazy dog"
+        is_pangram = verify_pangram(sentence)
+
+        self.assertTrue(is_pangram)
