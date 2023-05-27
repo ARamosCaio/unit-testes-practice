a4445497b7dd8822ef7aed19ace41a1b465f4cb9
Caio Ramos <aramoscaio@gmail.com>
2023-05-27 19:34:10 -0300

    feat: added sentence input to verify the test

--- test_verify_pangram.py
+++ test_verify_pangram.py
@@ -4,13 +4,9 @@ from pangram import verify_pangram
 class VerifyPangramTest(unittest.TestCase):
     def test_method(self):
         
-        sentence = "Not a pangram"
+        sentence = input('Insira a frase: ')
         is_pangram = verify_pangram(sentence)
 
         self.assertTrue(is_pangram)
-
-    def test_method(self):
-        sentence = "The quick brown fox jumps over a lazy dog"
-        is_pangram = verify_pangram(sentence)
-
-        self.assertTrue(is_pangram)
+        #sentence = "The quick brown fox jumps over a lazy dog"
+    
