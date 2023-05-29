8ef5bfcd418dd2ba5788bf8ae550210a0275d049
Caio Ramos <aramoscaio@gmail.com>
2023-05-29 20:24:10 -0300

    feat: added input to the function

--- pangram.py
+++ pangram.py
@@ -1,6 +1,7 @@
 import string
 
 def verify_pangram(sentence):
+    sentence = input('Insira a frase: ')
     for i in string.ascii_lowercase:
         if i not in sentence.lower():
             return False
