11892a43b3c34d8983eb3b18a669bc5fccc80c0b
Caio Ramos <aramoscaio@gmail.com>
2023-05-29 20:23:49 -0300

    feat: added second test

--- test_verify_pangram.py
+++ test_verify_pangram.py
@@ -3,10 +3,14 @@ from pangram import verify_pangram
 
 class VerifyPangramTest(unittest.TestCase):
     def test_method(self):
-        
         sentence = input('Insira a frase: ')
         is_pangram = verify_pangram(sentence)
 
-        self.assertTrue(is_pangram)
+        self.assertEqual(is_pangram, False)
         #sentence = "The quick brown fox jumps over a lazy dog"
     
+    def test_method(self):
+        sentence = input('Insira a frase: ')
+        is_pangram = verify_pangram(sentence)
+
+        self.assertEqual(is_pangram, True)
