325900ff40464e4e44d8576aeaeeee22fddb5703
Caio Vinícius Araújo Ramos <105233742+ARamosCaio@users.noreply.github.com>
2023-07-12 22:55:33 -0300

    Delete pangram.py

--- pangram.py
+++
@@ -1,8 +0,0 @@
-import string
-
-def verify_pangram(sentence):
-    sentence = input('Insira a frase: ')
-    for i in string.ascii_lowercase:
-        if i not in sentence.lower():
-            return False
-    return True
