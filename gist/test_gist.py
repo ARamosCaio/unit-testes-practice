a9e0f3ed9d335801797de88e46289de195c2123c
Caio Ramos <aramoscaio@gmail.com>
2023-10-26 00:32:49 -0300

    feat: Added fixture

--- gist/test_gist.py
+++ gist/test_gist.py
@@ -29,3 +29,21 @@ def test_with_custom_mark1() -> None:
 
 # Run "pytest . -v -p no:warnings -m slow" to run only the slow tests (tagged as slow)
 # Or Run "pytest . -v -p no:warnings -m not slow" to run all tests that aren't slow
+
+class Company:
+    def __init__(self, name: str, stock_symbol: str):
+        self.name = name
+        self.stock_symbol = stock_symbol
+
+    def __str__(self):
+        return f"{self.name}: {self.stock_symbol}"
+
+# A fixture is a function that might be called before and/or after running test functions
+@pytest.fixture 
+def company() -> Company:
+    return Company(name="Fiver", stock_symbol="FVRR") 
+
+def test_with_fixture(company: Company) -> None:
+    print(f"Printing {company} from fixture")
+
+# Run "pytest . -v -p no:warnings -s" to show what we printed
